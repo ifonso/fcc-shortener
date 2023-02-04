181b7b8b45dabf67bea97ea3e8ae38636e030851
unchainedDavid <afonso.script.sol@gmail.com>
2023-02-04 18:51:39 -0400

    fix: removing cors blocking at static files

--- src/index.ts
+++ src/index.ts
@@ -7,29 +7,30 @@ import routes from "./routes";
 const PORT = process.env.PORT || 443;
 const optioins: cors.CorsOptions = {
   origin: "*",
-  optionsSuccessStatus: 200
-}
+  optionsSuccessStatus: 200,
+};
 
 const app = express();
 
 dotenv.config();
 
 mongoose.set("strictQuery", false);
-mongoose.connect(process.env.MONGO_URI || "")
+mongoose
+  .connect(process.env.MONGO_URI || "")
   .then(() => {
     console.log("Database connected.");
   })
-  .catch(err => {
+  .catch((err) => {
     console.log("Error while connecting to database.");
     console.error(err);
     process.exit(1);
-  })
+  });
 
+app.use(cors(optioins));
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(optioins));
 app.use(routes);
 
 app.listen(PORT, () => {
   console.log(`Server listen at: [${PORT}]`);
-})
+});
